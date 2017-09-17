9cfd6d2237dd9dbc09718ce87406723ac54eb8ee
jani-123 <janina.rqa@gmail.com>
2017-09-16 19:38:44 -0500

    ultimas modificaciones - finalizado

--- js/main.js
+++ js/main.js
@@ -1,34 +1,31 @@
-var img = ["assets/images/portfolio/cabin.png","assets/images/portfolio/cake.png",
-           "assets/images/portfolio/circus.png","assets/images/portfolio/game.png",
-           "assets/images/portfolio/safe.png","assets/images/portfolio/submarine.png",];
-
-for(var i in img) 
-{ 
-   var secuencia = document.getElementById('fotos');
-   var divPicture = document.createElement('div'); 
-       divPicture.setAttribute('class','col-4 seccionImg');
-   var picture = document.createElement('img');
-       picture.setAttribute('class','images');
-       picture.setAttribute('src', img[i]);
-   divPicture.appendChild(picture); 
-   secuencia.appendChild(divPicture);
-}
-
-secuencia.addEventListener('click',mostrar);
-var modal = document.getElementById('myModal');
-var img;
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-
-function mostrar(event)
-{
-  img = event.target;
-  modal.style.display = "block";
-  modalImg.src = img.src;
-  captionText.innerHTML = img.alt;
-}
-
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function(){ 
-  modal.style.display = "none";
-}
+$(document).ready(function () {
+  var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+  $("#btnContacto").click(function () {
+    $(".error").remove();
+    if ($("#nombre").val() == "") {
+      $("#nombre").focus().after("<span class='error'>Ingrese su Nombre</span>");
+      return false;
+    } else if ($("#correo").val() == "" ) {
+      $("#correo").focus().after("<span class='error'>Ingrese un Correo Valido</span>");
+      return false;
+    } else if ($("#telefono").val() == "") {
+      $("#telefono").focus().after("<span class='error'>Ingrese su Telefono</span>");
+      return false;
+    } else if ($("#comentario").val() == "") {
+      $("#comentario").focus().after("<span class='error'>Ingrese un Comentario</span>");
+      return false;
+    }
+  });
+  $("#nombre, #comentario ,#telefono").keyup(function () {
+    if ($(this).val() != "") {
+      $(".error").fadeOut();
+      return false;
+    }
+  });
+  $("#correo").keyup(function () {
+    if ($(this).val() != "" && emailreg.test($(this).val())) {
+      $(".error").fadeOut();
+      return false;
+    }
+  });
+});
